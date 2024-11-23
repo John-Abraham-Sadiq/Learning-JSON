06ae00eeb0b441340e2846db148fe45bb0b6331c
asp <asp@aptech.com>
2024-11-23 18:57:23 +0500

    Learning JSON

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("js working")
+
+
+const obj={
+    "name" : "John",
+    "age" : 17,
+    "email" : "abc@gail.com",
+    "courses" : ["github","figma","xml"],
+
+    "School" : {
+        "name" : "Naveed Public Sec.School",
+        "Branch" : "Baldia Town"
+    }
+}
+
+console.log(obj);
